1d423e0ec268a05cc5fa39075b8d7c4ef7f0ada7
Vikas Gupta <Vikas.ymca@gmail.com>
2014-05-04 14:15:14 -0500

    Fine tuned Q4

--- rankall.R
+++ rankall.R
@@ -11,24 +11,25 @@ rankall  <- function(outcome, num = "best") {
                                 Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia = "character", 
                                 Hospital.Name = "character"))
   
+  # convert ratings to numeric
   df[, 11] <- suppressWarnings(as.numeric(df[, 11]))
   df[, 17] <- suppressWarnings(as.numeric(df[, 17]))
   df[, 23] <- suppressWarnings(as.numeric(df[, 23]))
   
+  # Create a subset dataframe for given outcome
   if (outcome == "heart attack") {
     df1 <- df[, c(2, 7, 11)]
-    names(df1)[3] <- "Rate"
   }else if (outcome == "heart failure") {
     df1 <- df[, c(2, 7, 17)]
-    names(df1)[3] <- "Rate"
   } else if (outcome == "pneumonia") {
     df1 <- df[, c(2, 7, 23)]
-    names(df1)[3] <- "Rate"
   }
   
   #Rename Column name of Hospital name to Hospital
   names(df1)[1]  <- "hospital"
   names(df1)[2]  <- "state"
+  names(df1)[3]  <- "Rate"
+  
   # Remove rows where Ratings are not known.
   df1 <- df1[!is.na(df1$Rate), ]
   #head(df1)
@@ -36,6 +37,7 @@ rankall  <- function(outcome, num = "best") {
   library(data.table)
   dt <- as.data.table(df1[,c("hospital","state","Rate")])
   #head(dt)
+  # Add ranking to the table
   dt1  <- dt[,list(hospital, rank = rank(Rate,ties.method = "first")), by = state]
   
   #=======================
